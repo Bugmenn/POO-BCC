b948d9f138073e5070392c5563a4ed988a636418
Bugmenn <125574692+Bugmenn@users.noreply.github.com>
2024-09-28 01:52:11 -0300

    Finalizado Lista 8

--- Arquivos/src/lista7B/TelaListar.java
+++ Arquivos/src/lista7B/TelaListar.java
@@ -2,20 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package lista7B.view;
-
-import javax.swing.JLabel;
+package lista7B;
 
 /**
  *
- * @author gbvanzuita
+ * @author gabri
  */
-public class TelaAluno extends javax.swing.JFrame {
+public class TelaListar extends javax.swing.JFrame {
 
     /**
-     * Creates new form Tela
+     * Creates new form TelaListar
      */
-    public TelaAluno() {
+    public TelaListar() {
         initComponents();
     }
 
@@ -28,12 +26,17 @@ public class TelaAluno extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
         jButton1 = new javax.swing.JButton();
 
-        jLabel1.setText("Teste");
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
-        jButton1.setText("jButton1");
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        jButton1.setText("Fechar");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
@@ -45,22 +48,22 @@ public class TelaAluno extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(150, 150, 150)
-                .addComponent(jLabel1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE)
+                .addContainerGap())
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(205, Short.MAX_VALUE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButton1)
-                .addGap(120, 120, 120))
+                .addGap(22, 22, 22))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(75, 75, 75)
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 248, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton1)
-                .addGap(29, 29, 29)
-                .addComponent(jLabel1)
-                .addContainerGap(157, Short.MAX_VALUE))
+                .addContainerGap(11, Short.MAX_VALUE))
         );
 
         pack();
@@ -70,11 +73,7 @@ public class TelaAluno extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         this.setVisible(false);
     }//GEN-LAST:event_jButton1ActionPerformed
-    
-    
-    public JLabel getTexto() {
-        return jLabel1;
-    }
+
     /**
      * @param args the command line arguments
      */
@@ -92,27 +91,27 @@ public class TelaAluno extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TelaAluno.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaListar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TelaAluno.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaListar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TelaAluno.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaListar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TelaAluno.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaListar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new TelaAluno().setVisible(true);
+                new TelaListar().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
 }
--- Arquivos/src/lista8/view/TelaTurma.java
+++ Arquivos/src/lista8/view/TelaTurma.java
@@ -2,14 +2,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package lista7B.view;
+package lista8.view;
+
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+import lista8.model.Titulacao;
+import lista8.model.Turma;
+import lista8.model.Turno;
+import lista8.model.Aluno;
+import lista8.model.Professor;
 
 /**
  *
  * @author gbvanzuita
  */
 public class TelaTurma extends javax.swing.JFrame {
-
+    
+    private Turma turma;
+    
     /**
      * Creates new form Tela2
      */
@@ -29,21 +39,23 @@ public class TelaTurma extends javax.swing.JFrame {
         buttonGroup1 = new javax.swing.ButtonGroup();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        jTfDisciplina = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        jRadioButton1 = new javax.swing.JRadioButton();
-        jRadioButton2 = new javax.swing.JRadioButton();
-        jRadioButton3 = new javax.swing.JRadioButton();
+        jRbMatutino = new javax.swing.JRadioButton();
+        jRbVespertino = new javax.swing.JRadioButton();
+        jRbNoturno = new javax.swing.JRadioButton();
         jPanel2 = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
+        jTfNome = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         jComboBox1 = new javax.swing.JComboBox<>();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
+        jTfEmail = new javax.swing.JTextField();
+        jBtIncluirAluno = new javax.swing.JButton();
+        jBtListar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(401, 350));
+        setPreferredSize(new java.awt.Dimension(401, 390));
 
         jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Turma"));
 
@@ -51,14 +63,14 @@ public class TelaTurma extends javax.swing.JFrame {
 
         jLabel2.setText("Turno:");
 
-        buttonGroup1.add(jRadioButton1);
-        jRadioButton1.setText("Matutino");
+        buttonGroup1.add(jRbMatutino);
+        jRbMatutino.setText("Matutino");
 
-        buttonGroup1.add(jRadioButton2);
-        jRadioButton2.setText("Vespertino");
+        buttonGroup1.add(jRbVespertino);
+        jRbVespertino.setText("Vespertino");
 
-        buttonGroup1.add(jRadioButton3);
-        jRadioButton3.setText("Noturno");
+        buttonGroup1.add(jRbNoturno);
+        jRbNoturno.setText("Noturno");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -70,15 +82,15 @@ public class TelaTurma extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextField1))
+                        .addComponent(jTfDisciplina))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jRadioButton1)
+                        .addComponent(jRbMatutino)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jRadioButton2)
+                        .addComponent(jRbVespertino)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jRadioButton3)
+                        .addComponent(jRbNoturno)
                         .addGap(0, 96, Short.MAX_VALUE)))
                 .addContainerGap())
         );
@@ -87,13 +99,13 @@ public class TelaTurma extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jTfDisciplina, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(jRadioButton1)
-                    .addComponent(jRadioButton2)
-                    .addComponent(jRadioButton3))
+                    .addComponent(jRbMatutino)
+                    .addComponent(jRbVespertino)
+                    .addComponent(jRbNoturno))
                 .addContainerGap(7, Short.MAX_VALUE))
         );
 
@@ -105,6 +117,8 @@ public class TelaTurma extends javax.swing.JFrame {
 
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Graduação", "Mestrado", "Doutorado" }));
 
+        jLabel5.setText("Email:");
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
@@ -113,38 +127,51 @@ public class TelaTurma extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addComponent(jLabel3)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextField2))
-                    .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(jLabel4)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 0, Short.MAX_VALUE)))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jTfNome)
+                            .addComponent(jTfEmail))))
                 .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGap(6, 6, 6)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTfNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(jTfEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
                     .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(0, 12, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        jButton1.setText("Incluir aluno");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jBtIncluirAluno.setText("Incluir aluno");
+        jBtIncluirAluno.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                jBtIncluirAlunoActionPerformed(evt);
             }
         });
 
-        jButton2.setText("Listar dados da turma");
+        jBtListar.setText("Listar dados da turma");
+        jBtListar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBtListarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -156,10 +183,9 @@ public class TelaTurma extends javax.swing.JFrame {
                     .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(6, 6, 6)
-                        .addComponent(jButton1)
+                        .addComponent(jBtIncluirAluno)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jButton2)
+                        .addComponent(jBtListar)
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
@@ -170,32 +196,72 @@ public class TelaTurma extends javax.swing.JFrame {
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
-                .addGap(133, 133, 133))
+                    .addComponent(jBtIncluirAluno)
+                    .addComponent(jBtListar))
+                .addGap(154, 154, 154))
         );
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        TelaAluno tela = new TelaAluno();
-        tela.setVisible(true);
-        jLabel1.setText(tela.getTexto().getText());
-        String texto = jComboBox1.getSelectedItem().toString().toUpperCase();
-        switch (texto) {
-            case "GRADUAÇÃO":
-                jLabel2.setText("funciona");
-                break;
-            default:
-                throw new AssertionError();
+    private void jBtIncluirAlunoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtIncluirAlunoActionPerformed
+        try {
+            
+            String disciplina = jTfDisciplina.getText().trim();
+            
+            Turno turno = jRbMatutino.isSelected() ? Turno.MATUTINO : (jRbVespertino.isSelected() ? Turno.VESPERTINO : Turno.NOTURNO);
+                        
+            ArrayList<Aluno> alunos = new ArrayList<>();
+            
+            String texto = jComboBox1.getSelectedItem().toString();
+            Titulacao titulacao = texto.equals("Graduação") ? Titulacao.GRADUACAO : (texto.equals("Mestrado") ? Titulacao.MESTRADO : Titulacao.DOUTORADO);
+            
+            Professor professor = new Professor(jTfNome.getText().trim(), jTfEmail.getText().trim(), titulacao);
+                        
+            this.turma = new Turma(disciplina, turno, alunos, professor);
+            
+            TelaAluno tela = new TelaAluno(this);
+            tela.setVisible(true);
+             
+        } catch (IllegalArgumentException error) {
+            JOptionPane.showMessageDialog(null, error.getMessage());
         }
-        jTextField2.setText(texto);
-    }//GEN-LAST:event_jButton1ActionPerformed
 
+    }//GEN-LAST:event_jBtIncluirAlunoActionPerformed
+
+    private void jBtListarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtListarActionPerformed
+        TelaListar telaListar = new TelaListar(this);
+       telaListar.setVisible(true);
+    }//GEN-LAST:event_jBtListarActionPerformed
+    
+    public Turma getTurma() {
+        return this.turma;
+    }
+    
+    public String Listar() {
+        String texto = "";
+        texto += "Disciplina: " + this.turma.getDisciplina() + "\n"
+                + "Turno: " + this.turma.getTurno().toString() + "\n"
+                + "------------------------\n"
+                + "Professor: " + this.turma.getProfessor().getNome() + "\n"
+                + "Titulação: " + this.turma.getProfessor().getTitulacao().toString() + "\n"
+                + "------------------------\n"
+                + "Total de alunos: " + this.turma.getAlunos().size() + "\n";
+        
+        for (Aluno aluno : this.turma.getAlunos()) {
+            texto += aluno.getNome() + " " + String.valueOf(aluno.getMatricula()) + " - Nota ENEM: " + String.valueOf(aluno.getNotaEnem()) + "\n";
+        }
+        
+        Aluno melhorAluno = this.turma.obterAlunoMelhorNotaEnem();
+                
+        texto += "Aluno(a) com melhor nota: " + melhorAluno.getNome() + " - (" + String.valueOf(melhorAluno.getNotaEnem()) + ")";
+        
+        return texto;
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -223,6 +289,8 @@ public class TelaTurma extends javax.swing.JFrame {
         }
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -234,19 +302,21 @@ public class TelaTurma extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.ButtonGroup buttonGroup1;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jBtIncluirAluno;
+    private javax.swing.JButton jBtListar;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JRadioButton jRadioButton1;
-    private javax.swing.JRadioButton jRadioButton2;
-    private javax.swing.JRadioButton jRadioButton3;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
+    private javax.swing.JRadioButton jRbMatutino;
+    private javax.swing.JRadioButton jRbNoturno;
+    private javax.swing.JRadioButton jRbVespertino;
+    private javax.swing.JTextField jTfDisciplina;
+    private javax.swing.JTextField jTfEmail;
+    private javax.swing.JTextField jTfNome;
     // End of variables declaration//GEN-END:variables
 }
